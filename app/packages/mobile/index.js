1e762dfa3a781f155592d77c126cb24222ef4aea
Andrej Mihajlov <and@mullvad.net>
2018-07-31 15:35:48 +0200

    Basic configuration for react-native project

--- app/packages/mobile/index.js
+++ app/packages/mobile/index.js
@@ -1,7 +1,7 @@
 // @flow
-import * as React from 'react';
-import RX from 'reactxp';
-import App from './App';
 
-RX.App.initialize(true, true);
-RX.UserInterface.setMainView(<App />);
+import { AppRegistry } from 'react-native';
+import App from './App';
+import { name as appName } from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
